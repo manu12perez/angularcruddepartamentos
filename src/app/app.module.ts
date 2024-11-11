b211b97d86d0a15365a024e90f2d91c41e9f62e7
manu12perez <manu12perez@gmail.com>
2024-11-11 14:12:21 +0100

    Crud Departamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { provideHttpClient } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ModelsComponent } from './components/models/models.component';
+
+import { ServiceDepartamentos } from '../services/service.departamentos';
+import { CreateComponent } from './components/create/create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    ModelsComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [provideHttpClient(), ServiceDepartamentos],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
